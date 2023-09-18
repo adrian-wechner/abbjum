d6cf1e27155429d31efd9e2f570975dc7bd7bbcd
Adrian Wechner <adrian.wechner@gmail.com>
2023-09-18 17:13:18 -0600

    associations checklists

--- app/models/checklist_record.rb
+++ app/models/checklist_record.rb
@@ -1,2 +1,6 @@
 class ChecklistRecord < ApplicationRecord
+  belongs_to :checklist
+  belongs_to :line
+  belongs_to :user_start, class_name: "User", foreign_key: "user_start_id"
+  belongs_to :user_complete, class_name: "User", foreign_key: "user_complete_id"
 end
