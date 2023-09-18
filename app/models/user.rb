c680428c7dce412c97c4d546b9d5f01865a05449
Adrian Wechner <adrian.wechner@gmail.com>
2023-09-18 17:16:35 -0600

    associations for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  
+  has_many :checklist_starts, class_name: "ChecklistRecord", foreign_key: "user_start_id"
+  has_many :checklist_completes, class_name: "ChecklistRecord", foreign_key: "user_complete_id"
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
